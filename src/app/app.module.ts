327fa4307b0e2765f7ffbcbdf12ebb1ddc70295e
Francis <francisromasanta12@gmail.com>
2024-04-27 13:18:38 +0800

    generate home, weather, and landing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HomeComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
